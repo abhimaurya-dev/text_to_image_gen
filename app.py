ead0de7b25b9c99940c384c719a516a7db3c10ba
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2024-10-13 23:58:03 +0530

    fixed redirect and added loading spinner

--- app.py
+++ app.py
@@ -19,22 +19,37 @@ class Config:
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 pipe = pipe.to(Config.DEVICE)
 
-def generate_image(prompt):
+def generate_img(prompt):
     images = pipe([prompt], num_inference_steps=Config.NUM_INFERENCE_STEPS).images
     return images[0]
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        prompt = request.form.get("prompt")
-        image = generate_image(prompt)
-        file_path = os.path.join(Config.SAVE_PATH, f"{prompt.replace(' ', '_')}.png")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     if request.method == "POST":
+#         prompt = request.form.get("prompt")
+#         image = generate_image(prompt)
+#         file_path = os.path.join(Config.SAVE_PATH, f"{prompt.replace(' ', '_')}.png")
         
-        # Save image to static/images directory
-        image.save(file_path)
+#         # Save image to static/images directory
+#         image.save(file_path)
         
-        return render_template("index.html", generated_image=file_path, prompt=prompt)
-    return render_template("index.html", generated_image=None)
+#         return render_template("index.html", generated_image=file_path, prompt=prompt)
+#     return render_template("index.html", generated_image=None)
+
+@app.route('/generate', methods=['POST'])
+def generate_image():
+    prompt = request.form.get("prompt")
+    
+    image = generate_img(prompt)
+    file_path = os.path.join(Config.SAVE_PATH, f"{prompt.replace(' ', '_')}.png")
+    
+    # Save image to static/images directory
+    image.save(file_path)
+    return send_file(file_path, mimetype='image/png')
 
 @app.route("/download/<filename>")
 def download_image(filename):
